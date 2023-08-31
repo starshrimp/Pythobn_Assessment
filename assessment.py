f76f0f7bffe10769a751bd555f6126ba47ed7e24
starshrimp <60508037+starshrimp@users.noreply.github.com>
2023-08-31 21:41:26 +0200

    Delete assessment.py

--- assessment.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('https://raw.githubusercontent.com/stawiskm/pythoncourse/teacher/Code_Examples/Day%204/Data/cardio.csv')
-
-print(data.dtypes)
-print(data.head(10))
